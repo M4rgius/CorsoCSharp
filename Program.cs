b5fd5622ea7b6e28f82fcf924b46895f9463f249
M4rgius <79052176+M4rgius@users.noreply.github.com>
2023-12-06 11:07:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CorsoCSharp
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inizializzazione
+            string nome = "Luca";
+
+            //dichiarazione 
+            string cognome;
+
+            //assegnazione 
+            cognome = "Rossi";
+
+            //riassegnazione 
+            nome = "Marco";
+
+            int eta = 19;
+
+           
+
+            Console.WriteLine("Ciao mi chiamo " + nome + cognome + "ed ho " + eta + " anni");
+            Console.Read();
+
+
+        }
+
+
+
+
+    }
+}
