058cfdda06fb78e39c516ac356df33679b89ce8e
M4rgius <79052176+M4rgius@users.noreply.github.com>
2023-12-07 09:47:09 +0100

    Create Dati.cs
    Utilizzo dei dati

---
+++ Dati.cs
@@ -0,0 +1,23 @@
+// See https://aka.ms/new-console-template for more information
+namespace Corso
+{
+    class Program
+    {
+static void Main(string[] args)
+        {
+            //stringhe + char
+            string a = "Hello";
+            char b = 'C';
+
+            //numeri interi
+            int c = 1000000000;
+            long d =1000000000000000000;
+
+
+            //numeri con virgola
+            double e=3.4; //conserva da 15 a 17
+            float f=3.5f; //conserva da 6 a 9 numeri
+            decimal g = 4.5m;//conserva da 28 a 29 numeri         
+           }
+}
+}
